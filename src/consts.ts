2332e1fa506570c44b5b49a94df69a8303575a81
Komang Suartana <kmdavidds@gmail.com>
2026-08-12 16:49:53 +0700

    chore: update astro config

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Komang David's Site";
-export const SITE_DESCRIPTION = 'Welcome!';
+export const SITE_TITLE = 'Komang David, Software Engineer';
+export const SITE_DESCRIPTION =
+  'Komang David is a software engineer that is building reliable software with Go, TypeScript, and Python.';
